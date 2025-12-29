0a0aed8d124d1779facc38f33a852d3b42670acd
ImSota <sotahoriguchi1229@gmail.com>
2025-12-30 01:08:32 +0900

    test

--- src/zmk-feature-twistsync.c
+++ src/zmk-feature-twistsync.c
@@ -2,6 +2,7 @@
 
 #include <zephyr/device.h>
 #include <zephyr/input/input.h>
+#include <zmk/behavior.h> // input pprocessor.hが読み込めないならこれはどうかな？
 #include <zmk/input_processor.h>
 #include <stdlib.h> // abs() 用
 
