625a7d2bcbc0167c82c52091a157eb2c818d404d
ImSota <sotahoriguchi1229@gmail.com>
2025-12-30 17:22:41 +0900

    test

--- src/zmk-feature-twistsync.c
+++ src/zmk-feature-twistsync.c
@@ -11,7 +11,7 @@ LOG_MODULE_REGISTER(zmk_input_processor_twist_sync, CONFIG_INPUT_LOG_LEVEL);
 
 /* 判定用定数（必要に応じて調整） */
 #define SCROLL_THRESHOLD_MIN 100   // これ以下の動きを「微小操作」とみなす
-#define CURSOR_BLOCK_LIMIT  10    // 微小操作中、これ以上のカーソル移動軸の動きがあればブロック
+#define CURSOR_BLOCK_LIMIT  50    // 微小操作中、これ以上のカーソル移動軸の動きがあればブロック
 #define RATIO_MARGIN        2    // 高速域での比率（XがYの何倍以上必要か）
 
 struct twist_sync_config {
